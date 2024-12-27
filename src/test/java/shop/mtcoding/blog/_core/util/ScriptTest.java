0f0c0ea2b3df8352c383a9a6feae371f1ce6a123
thdus1323 <thdus1323@naver.com>
2024-12-27 13:32:43 +0900

    시작

--- src/test/java/shop/mtcoding/blog/_core/util/ScriptTest.java
+++ src/test/java/shop/mtcoding/blog/_core/util/ScriptTest.java
@@ -1,11 +1,11 @@
-package shop.mtcoding.blog._core.utill;
+package shop.mtcoding.blog._core.util;
 
 import org.junit.jupiter.api.Test;
 
 public class ScriptTest {
 
     @Test
-    public void back_test(){
+    public void back_test() {
         String result = Script.back("권한이없어요");
         System.out.println(result);
     }
